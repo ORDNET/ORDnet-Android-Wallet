e711b9995927ba8dfb9abf3439b24ff3c3eb9d90
ORDNET <138910187+ORDNET@users.noreply.github.com>
2026-08-13 17:35:11 +0200

    Bump to 3.4.1
     (versionCode 341)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.ordnet.wallet"
         minSdk = 26
         targetSdk = 36
-        versionCode = 340
-        versionName = "3.4"
+        versionCode = 341
+        versionName = "3.4.1"
     }
 
     buildTypes {
